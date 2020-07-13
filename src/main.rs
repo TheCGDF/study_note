bbfffa1fb3f89de2a3db359f265860556b21cba0
TheCGDF <17256673+TheCGDF@users.noreply.github.com>
2020-07-14 00:45:05 +0900

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 pub mod config;
 
-use telegram_bot::{UpdateKind, MessageKind, Api, MessageOrChannelPost, ForwardMessage, ChatId, CanReplySendMessage, Message, MessageId};
+use telegram_bot::{UpdateKind, MessageKind, Api, MessageOrChannelPost, ForwardMessage, ChatId, CanReplySendMessage, Message, MessageId, GetChatAdministrators, ChatMember, ToMessageId, DeleteMessage};
 use futures::StreamExt;
 use rand::Rng;
 
@@ -20,7 +20,9 @@ async fn main() {
                     api.send(update_message.text_reply("笔记本对你上锁了哦")).await.unwrap();
                     continue;
                 }
-                //todo get admins
+                let admins: Vec<ChatMember> = api.send(
+                    GetChatAdministrators::new(ChatId::new(config.group))
+                ).await.unwrap();
                 match data.replace("@study_note_bot", "").trim() {
                     "/id" => {
                         let id: i64 = update_message.chat.id().into();
@@ -28,22 +30,10 @@ async fn main() {
                     }
                     "/note" => {
                         if let Some(reply) = &update_message.reply_to_message {
-                            let last_message: Message = match &**reply {
-                                MessageOrChannelPost::ChannelPost(channel_post) => {
-                                    api.send(ForwardMessage::new(
-                                        channel_post.id,
-                                        &channel_post.chat,
-                                        ChatId::new(config.group)))
-                                }
-                                MessageOrChannelPost::Message(message) => {
-                                    api.send(ForwardMessage::new(
-                                        message.id,
-                                        &message.chat,
-                                        ChatId::new(config.group),
-                                    ))
-                                }
-                            }.await.unwrap();
-
+                            let last_message: Message = api.send(ForwardMessage::new(
+                                reply.to_message_id(),
+                                &update_message.chat,
+                                ChatId::new(config.group))).await.unwrap();
                             notes.push(last_message.id.into());
                             config.last = last_message.id.into();
                             config.save();
@@ -51,6 +41,9 @@ async fn main() {
                         }
                     }
                     "/review" => {
+                        if notes.is_empty() {
+                            api.send(&update_message.text_reply("还没有笔记哦")).await.unwrap();
+                        }
                         api.send(ForwardMessage::new(
                             MessageId::new(notes[rng.gen_range(0, notes.len())]),
                             ChatId::new(config.group.into()),
@@ -58,6 +51,9 @@ async fn main() {
                         )).await.unwrap();
                     }
                     "/cram" => {
+                        if notes.is_empty() {
+                            api.send(&update_message.text_reply("还没有笔记哦")).await.unwrap();
+                        }
                         for _ in 1..5 {
                             api.send(ForwardMessage::new(
                                 MessageId::new(notes[rng.gen_range(0, notes.len())]),
@@ -71,9 +67,28 @@ async fn main() {
                         if chat != config.group {
                             continue;
                         }
-                        //todo delete
+                        if let Some(reply) = &update_message.reply_to_message {
+                            let reply_id: i64 = reply.to_message_id().into();
+                            notes.retain(|&note| note != reply_id);
+                            api.send(
+                                DeleteMessage::new(ChatId::new(config.group), MessageId::new(reply_id))
+                            ).await.unwrap();
+                        }
+                        api.send(
+                            DeleteMessage::new(ChatId::new(config.group), update_message.id)
+                        ).await.unwrap();
                     }
                     "/lock" => {
+                        let mut is_admin = false;
+                        for admin in admins {
+                            if admin.user == update_message.from {
+                                is_admin = true;
+                                break;
+                            }
+                        }
+                        if !is_admin {
+                            continue;
+                        }
                         if let Some(reply) = &update_message.reply_to_message {
                             match &**reply {
                                 MessageOrChannelPost::ChannelPost(_) => {
@@ -88,6 +103,16 @@ async fn main() {
                         }
                     }
                     "/unlock" => {
+                        let mut is_admin = false;
+                        for admin in admins {
+                            if admin.user == update_message.from {
+                                is_admin = true;
+                                break;
+                            }
+                        }
+                        if !is_admin {
+                            continue;
+                        }
                         if let Some(reply) = &update_message.reply_to_message {
                             match &**reply {
                                 MessageOrChannelPost::ChannelPost(_) => {
@@ -95,7 +120,7 @@ async fn main() {
                                 }
                                 MessageOrChannelPost::Message(message) => {
                                     let user_locked: i64 = message.from.id.into();
-                                    config.locks.retain(|item| *item == user_locked);
+                                    config.locks.retain(|&item| item != user_locked);
                                     config.save();
                                     api.send(&update_message.text_reply("笔记已对其上锁。。。")).await.unwrap();
                                 }
